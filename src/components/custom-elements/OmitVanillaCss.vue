e4244c10258fe1f6fb1403f679671160bfd9271d
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2024-12-12 19:21:58 -0300

    Use onBeforeUnmount instead of onBeforeRouteLeave

--- src/components/custom-elements/OmitVanillaCss.vue
+++ src/components/custom-elements/OmitVanillaCss.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { useVanillaCss } from '@/lib/shared-globals';
 import { onBeforeUnmount } from 'vue';
-import { onBeforeRouteLeave } from 'vue-router';
 
 useVanillaCss.value = false;
 
-onBeforeRouteLeave(() => {
+// was onBeforeRouteLeave
+onBeforeUnmount(() => {
     useVanillaCss.value = true;
 });
 
