7663d741c7af3f48fdcee391d35eb8d1ed84c38b
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2024-12-12 18:23:01 -0300

    Remove custom elements

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
