74c50bd5c5f3e9cd12e47695e5def692b49b6923
deliciously-default <181034365+deliciously-default@users.noreply.github.com>
2024-12-14 10:02:20 -0300

    Add unplugin-vue-components bc i cant figure out how to make volar not use global imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,6 +20,14 @@ export default defineConfig({
         }),
         vueJsx(),
         vueDevTools(),
+        Components({
+            resolvers: [
+              (componentName) => {
+                if (componentName.startsWith('Va'))
+                  return { name: componentName, from: 'vuestic-ui' };
+              },
+            ],
+        }),
 
         // // @quasar/plugin-vite options list:
         // // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
