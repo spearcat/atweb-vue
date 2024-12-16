d56d3da15a64c736be5bb054e8ef05938d8c81c7
spearcat <spearcat@users.noreply.github.com>
2024-12-16 11:08:51 -0300

    Fix oauth...

--- src/lib/atproto/oauth.ts
+++ src/lib/atproto/oauth.ts
@@ -1,7 +1,6 @@
 import { configureOAuth, createAuthorizationUrl, finalizeAuthorization, getSession, OAuthUserAgent, resolveFromIdentity } from '@atcute/oauth-browser-client';
 import type { FetchHandlerObject } from '@atcute/client';
 import type { At } from '@atcute/client/lexicons';
-import { useEventListener } from '@vueuse/core';
 
 configureOAuth({
     metadata: {
@@ -29,20 +28,12 @@ export class AtpOauthClient {
         // recommended to wait for the browser to persist local storage before proceeding
         await new Promise(resolve => setTimeout(resolve, 200));
 
-        const hashPromise = Promise.race([
-            new Promise(resolve => useEventListener(window, 'message', event => {
-                const hash = event.data as string;
-                resolve(hash);
-            })),
-            new Promise(resolve => setTimeout(resolve, 180000)) // 3 minutes
-        ]);
-
         // redirect the user to sign in and authorize the app
-        window.open(authUrl, '_blank');
+        window.open(authUrl, '_blank', 'noopener,noreferrer');
 
         // TODO setup a redirect instead
-        const hash = (await hashPromise) as string | undefined;
-        if (!hash) throw new Error('Authentication timed out');
+        const hash = prompt('Input the code that was displayed on the page');
+        if (!hash) throw new Error('User cancelled authentication');
 
         // `createAuthorizationUrl` asks for the server to redirect here with the
         // parameters assigned in the hash, not the search string.
