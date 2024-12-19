24258f9ca710ea3b461c0929004ead472d42549e
spearcat <spearcat@users.noreply.github.com>
2024-12-19 13:53:33 -0300

    Remove extraComponents (as no longer using vuestic)

--- src/lib/markdown/components.tsx
+++ src/lib/markdown/components.tsx
@@ -30,34 +30,6 @@ interface Context {
     slots: { [name: string]: Slot };
 }
 
-const extraComponents: Record<
-    string,
-    (props: Props, context: Context) => JSX.Element
-> = {
-    hr: () => <hr class="va-separator" />,
-    h1: (_, { slots }) => <h1 class="va-h1">{slots.default?.()}</h1>,
-    h2: (_, { slots }) => <h2 class="va-h2">{slots.default?.()}</h2>,
-    h3: (_, { slots }) => <h3 class="va-h3">{slots.default?.()}</h3>,
-    h4: (_, { slots }) => <h4 class="va-h4">{slots.default?.()}</h4>,
-    h5: (_, { slots }) => <h5 class="va-h5">{slots.default?.()}</h5>,
-    h6: (_, { slots }) => <h6 class="va-h6">{slots.default?.()}</h6>,
-    ol: (_, { slots }) => <ol class="va-ordered">{slots.default?.()}</ol>,
-    ul: (_, { slots }) => <ul class="va-unordered">{slots.default?.()}</ul>,
-    blockquote: (_, { slots }) => <blockquote class="va-blockquote">{slots.default?.()}</blockquote>,
-    uppercase: (_, { slots }) => <span class="va-text-uppercase">{slots.default?.()}</span>,
-    lowercase: (_, { slots }) => <span class="va-text-lowercase">{slots.default?.()}</span>,
-    capitalize: (_, { slots }) => <span class="va-text-capitalize">{slots.default?.()}</span>,
-    highlighted: (_, { slots }) => <span class="va-text-highlighted">{slots.default?.()}</span>,
-    left: (_, { slots }) => <span class="va-text-left">{slots.default?.()}</span>,
-    right: (_, { slots }) => <span class="va-text-right">{slots.default?.()}</span>,
-    center: (_, { slots }) => <span class="va-text-center">{slots.default?.()}</span>,
-    justify: (_, { slots }) => <span class="va-text-justify">{slots.default?.()}</span>,
-    truncate: (_, { slots }) => <span class="va-text-truncate">{slots.default?.()}</span>,
-    'no-wrap': (_, { slots }) => <span class="va-text-no-wrap">{slots.default?.()}</span>,
-
-    p: (_, { slots }) => <p class="va-paragraph">{slots.default?.()}</p>,
-};
-
 export const components: MDXComponents = {
     img: AtImg,
     link: AtLink,
@@ -76,6 +48,5 @@ export const components: MDXComponents = {
     LesbiBadge: LesbiBadge,
     Badge: EightyEightThirtyOneBadge,
     Userbar: UserbarBadge,
-    RingLink: RingLink,
-    ...(extraComponents as unknown as MDXComponents)
+    RingLink: RingLink
 };
